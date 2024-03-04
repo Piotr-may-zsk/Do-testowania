aff1756fe9d5670b0dbb509204372de45efba836
Piotr-may-zsk <piotr.may@uczen.zsk.poznan.pl>
2024-03-04 12:32:44 +0100

    Add project files.

---
+++ ClassLibrary1/BaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Klasa bazowa
+    /// </summary>
+    public abstract class BaseClass
+    {
+        /// <summary>
+        /// Funkcja testoa
+        /// </summary>
+        /// <param name="asd"></param>
+        /// <returns>Liczba calkowita</returns>
+        public abstract int TestowaFunkcja(int asd);
+
+        // TODO: coś się doda
+    }
+}
---
+++ ClassLibrary1/TestowaKlasa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Komentasz
+    /// </summary>
+    public class TestowaKlasa : BaseClass
+    {
+        /// <inheritdoc/>
+        public override int TestowaFunkcja(int asd)
+        {
+            // zwraca 0 bo tak
+            return 0;
+        }
+    }
+}
