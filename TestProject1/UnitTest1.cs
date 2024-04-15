521daf8135cc338161c63a20a5f82b0eb0dbf16b
Piotr-may-zsk <piotr.may@uczen.zsk.poznan.pl>
2024-04-15 14:40:59 +0200

    t2

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,5 @@
+using ClassLibrary1;
+
 namespace TestProject1
 {
     public class UnitTest1
@@ -6,6 +8,14 @@ namespace TestProject1
         public void Test1()
         {
             Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            TestowaKlasa t = new TestowaKlasa();
+            var x = t.TestowaFunkcja(1);
+            Assert.Equal(0, x);
         }
     }
 }
